d39946b1a96fc0a94fcc96be1c8d2040df7b7208
kimuky <xogus559@gmail.com>
2024-11-15 13:58:05 +0900

    fix: 리드미 수정 및 필요없는 리포지토리 코드 삭제

--- src/main/java/com/example/schedulemanagementdevelop/repository/CommentRepository.java
+++ src/main/java/com/example/schedulemanagementdevelop/repository/CommentRepository.java
@@ -2,13 +2,10 @@ package com.example.schedulemanagementdevelop.repository;
 
 
 import com.example.schedulemanagementdevelop.entity.Comment;
-import com.example.schedulemanagementdevelop.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByScheduleId(Long findSchedule);
-
-    List<Comment> findBySchedule(Schedule findSchedule);
 }
